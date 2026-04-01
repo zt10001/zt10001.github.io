40258d64fe5542b800c27ac21620516256f28d84
zt10001 <ztao10001@gmail.com>
2026-04-01 19:56:24 +0000

    Deploying to gh-pages from @ zt10001/zt10001.github.io@66dc6ef383d03372b57e25d6fb9cafb7200e0eef 🚀

--- assets/js/photoswipe-setup.js
+++ assets/js/photoswipe-setup.js
@@ -1,5 +1,5 @@
-import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@{{version}}/dist/photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@{{version}}/dist/photoswipe.esm.min.js";
+import PhotoSwipeLightbox from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe-lightbox.esm.min.js";
+import PhotoSwipe from "https://cdn.jsdelivr.net/npm/photoswipe@5.4.4/dist/photoswipe.esm.min.js";
 const photoswipe = new PhotoSwipeLightbox({
   gallery: ".pswp-gallery",
   children: "a",
